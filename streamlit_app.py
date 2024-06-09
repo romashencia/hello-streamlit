16a9d0c9429875388ba4b15f13f43dd7eea3f6b9
romashencia <kpr.rom@mail.ru>
2024-06-09 21:30:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,29 +15,29 @@ if uploaded_file is not None:
     data = pd.read_excel(uploaded_file)
     st.write(data)
 
-for target_object_type in ["здание", "нежилое помещение", "земельный участок"]:
-  target_entries = data[data["Тип объекта"] == target_object_type]
-
-  target_entries = target_entries[~target_entries["Начальная цена за квадратный метр"].isna()]
-
-  fig = px.density_mapbox(
-      target_entries,
-      lat="Широта",
-      lon="Долгота",
-      z="Начальная цена за квадратный метр",
-      radius=20,
-      zoom=0,
-      title=f"Тип объекта: {target_object_type}",
-      color_continuous_scale = px.colors.sequential.Plasma,
-      opacity=0.8,
-      mapbox_style="open-street-map"
-    )
-
-  # fig = px.scatter_mapbox(
-  #     target_entries, lat="Широта", lon="Долгота", color="Начальная цена за квадратный метр",
-  #     hover_name="Начальная цена за квадратный метр",
-  #     color_continuous_scale = px.colors.sequential.Viridis, opacity=0.5,
-  #     title=target_object_type)
+    for target_object_type in ["здание", "нежилое помещение", "земельный участок"]:
+      target_entries = data[data["Тип объекта"] == target_object_type]
+    
+      target_entries = target_entries[~target_entries["Начальная цена за квадратный метр"].isna()]
+    
+      fig = px.density_mapbox(
+          target_entries,
+          lat="Широта",
+          lon="Долгота",
+          z="Начальная цена за квадратный метр",
+          radius=20,
+          zoom=0,
+          title=f"Тип объекта: {target_object_type}",
+          color_continuous_scale = px.colors.sequential.Plasma,
+          opacity=0.8,
+          mapbox_style="open-street-map"
+        )
+    
+      # fig = px.scatter_mapbox(
+      #     target_entries, lat="Широта", lon="Долгота", color="Начальная цена за квадратный метр",
+      #     hover_name="Начальная цена за квадратный метр",
+      #     color_continuous_scale = px.colors.sequential.Viridis, opacity=0.5,
+      #     title=target_object_type)
 
   fig.update_layout(mapbox_style="open-street-map")
 
