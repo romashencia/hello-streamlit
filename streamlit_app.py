2dfdf347af06fde7b3fa395324f18c7af72a7c46
romashencia <kpr.rom@mail.ru>
2024-06-09 21:59:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,29 +10,30 @@ MAPBOX_TOKEN = "pk.eyJ1IjoibmJhcnlraW4iLCJhIjoiY2xzc3R2c2ZvMHlweDJscWkxcWc3bG1ta
 px.set_mapbox_access_token(MAPBOX_TOKEN)
 
 data = pd.read_excel("агрегированные_сделки.xlsx")
+st.write(data)
 
-# Карта
-for target_object_type in ["здание", "нежилое помещение", "земельный участок"]:
-    target_entries = data[data["Тип объекта"] == target_object_type]
+# # Карта
+# for target_object_type in ["здание", "нежилое помещение", "земельный участок"]:
+#     target_entries = data[data["Тип объекта"] == target_object_type]
 
-    target_entries = target_entries[~target_entries["Начальная цена за квадратный метр"].isna()]
+#     target_entries = target_entries[~target_entries["Начальная цена за квадратный метр"].isna()]
 
-    fig = px.density_mapbox(
-      target_entries,
-      lat="Широта",
-      lon="Долгота",
-      z="Начальная цена за квадратный метр",
-      center=dict(lat=59.95, lon=30.4), 
-      zoom=9,
-      radius=20,
-      title=f"Тип объекта: {target_object_type}",
-      color_continuous_scale = px.colors.sequential.Plasma,
-      opacity=0.8,
-      mapbox_style="open-street-map"
-    )
+#     fig = px.density_mapbox(
+#       target_entries,
+#       lat="Широта",
+#       lon="Долгота",
+#       z="Начальная цена за квадратный метр",
+#       center=dict(lat=59.95, lon=30.4), 
+#       zoom=9,
+#       radius=20,
+#       title=f"Тип объекта: {target_object_type}",
+#       color_continuous_scale = px.colors.sequential.Plasma,
+#       opacity=0.8,
+#       mapbox_style="open-street-map"
+#     )
     
-    st.plotly_chart(fig, use_container_width=True)
-    fig.update_layout(mapbox_style="open-street-map")
+#     st.plotly_chart(fig, use_container_width=True)
+#     fig.update_layout(mapbox_style="open-street-map")
 
 # # Относительный рост
 # MONTH_NAMES = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
