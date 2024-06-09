a69371eab9fbb3633a1858e4132a91656e321e5a
romashencia <kpr.rom@mail.ru>
2024-06-09 22:25:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
-# st.pyplot(fig)
+st.pyplot(fig)
 # import streamlit as st
 # import pandas as pd
 # from io import StringIO
