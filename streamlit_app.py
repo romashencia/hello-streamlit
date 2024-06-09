488a222524e77addf3735ee22ae7f7b38ef9a34b
romashencia <kpr.rom@mail.ru>
2024-06-09 21:43:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,24 @@ px.set_mapbox_access_token(MAPBOX_TOKEN)
 
 data = pd.read_excel("агрегированные_сделки.xlsx")
 
+# Карта
 for target_object_type in ["здание", "нежилое помещение", "земельный участок"]:
     target_entries = data[data["Тип объекта"] == target_object_type]
 
     target_entries = target_entries[~target_entries["Начальная цена за квадратный метр"].isna()]
 
     fig = px.density_mapbox(
-        target_entries,
-        lat="Широта",
-        lon="Долгота",
-        z="Начальная цена за квадратный метр",
-        radius=20,
-        zoom=0,
-        title=f"Тип объекта: {target_object_type}",
-        color_continuous_scale = px.colors.sequential.Plasma,
-        opacity=0.8,
-        mapbox_style="open-street-map"
+      target_entries,
+      lat="Широта",
+      lon="Долгота",
+      z="Начальная цена за квадратный метр",
+      center=dict(lat=59.95, lon=30.4), 
+      zoom=9,
+      radius=20,
+      title=f"Тип объекта: {target_object_type}",
+      color_continuous_scale = px.colors.sequential.Plasma,
+      opacity=0.8,
+      mapbox_style="open-street-map"
     )
     
     st.plotly_chart(fig)
