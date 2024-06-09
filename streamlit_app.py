28de6bf80156b8c29b9be93a588fd6986a3e8ff8
romashencia <kpr.rom@mail.ru>
2024-06-09 22:21:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
+sw.write("Hello, world!")
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
